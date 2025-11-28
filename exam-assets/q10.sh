53f4eaa6fff6624a43a10a8742ba7b239a99eaa5
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2025-11-28 12:47:58 +0300

    Create q10.sh

---
+++ exam-assets/q10.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd /tmp/.ques10/resources/
+
+# Define the commands to be executed
+command1="helm install security-alpha-apd --namespace=filtration-east ./filter-app"
+command2="helm install digi-locker-apd --namespace=secrets-global ./vault-app"
+command3="helm install web-dashboard-apd --namespace=view-global ./dashboard-app"
+command4="helm install atlanta-page-apd --namespace=catalog-eu-only ./catalog"
+
+sleep 2
+
+# Loop until all commands have executed successfully
+while true; do
+  # Execute the commands
+   $command1 && $command2 && $command3 && $command4
+  
+  # Check if any command failed
+  if [ $? -ne 0 ]; then
+    # If a command failed, print an error message and try once again and then exit the loop
+    echo "One or more commands failed. Retrying..."
+    $command1 && $command2 && $command3 && $command4
+    break
+  
+  else
+    # If all commands succeeded, exit the loop
+    break
+  fi
+done
+
+echo "All commands executed successfully."
+
+rm -rf /tmp/.ques10
