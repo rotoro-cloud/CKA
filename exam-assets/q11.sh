632222ca39842aa85d61e3761e9c0fd2de8d07fb
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2025-12-01 19:57:48 +0300

    Update q11.sh

--- exam-assets/q11.sh
+++ exam-assets/q11.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd /tmp/.script/resources
+cd /tmp/.script/exam-assets
 
 # Define the commands to deploy the helm chart
 helm install messenger-front-01 --namespace=default ./current-version
 
 # Moving the new version app directory 
-mv /tmp/.script/resources/nightly-build /root/
+mv /tmp/.script/exam-assets/nightly-build /root/
 
 # Removing the script 
 rm -rf /tmp/.script
